8b8e4d2070c3235b95361b1480a654e232554635
JustSteveKing <hello@thedevshed.co.uk>
2015-12-30 16:41:54 +0000

    repo.init()
    Initialisation of repo

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Bolt-Medium
-A Medium Inspired blogging theme for Bolt CMS
+## Bolt-Medium by JustSteveKing 
+===================
+
+![Preview](screenshots/homepage.png)
+
+### Introduction
+
+This is the Bolt-Medium theme for Bolt. A theme aimed at blogging with minimal pages. This is a Bolt port of the Jekyl and Ghost themes.
+
+### Configuration
+
+Have a look in the config.yml file and set a few of your own globals there. I have included the layouts directory so that you can upload this to your /web_root/files directory and have this running the same as my example. 
+
+Don't forget to clear yor cache every time you make changes.
+
+Check the screenshots folder to discover more of this theme.
+
+Any issues give me a shout on twitter https://www.twitter.com/JustSteveKing
---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".post-content").fitVids();
+
+        // Creates Captions from Alt tags
+        $(".post-content img").each(function() {
+            // Let's put a caption if there is one
+            if($(this).attr("alt"))
+              $(this).wrap('<figure class="image"></figure>')
+              .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
+        });
+
+    });
+
+    var $window = $(window),
+          $image = $('.post-image-image, .teaserimage-image');
+          $window.on('scroll', function() {
+            var top = $window.scrollTop();
+
+            if (top < 0 || top > 1500) { return; }
+            $image
+              .css('transform', 'translate3d(0px, '+top/3+'px, 0px)')
+              .css('opacity', 1-Math.max(top/700, 0));
+          });
+          $window.trigger('scroll');
+
+          var height = $('.article-image').height();
+          $('.post-content').css('padding-top', height + 'px');
+
+          $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+             && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html,body').animate({ scrollTop: target.offset().top }, 500);
+                return false;
+              }
+            }
+          });
+
+}(jQuery));
